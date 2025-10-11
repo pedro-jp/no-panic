ba07ed10db881c856db7c0049066f6ab9d8b1bbd
pedro-jp <joaopedroc035@gmail.com>
2025-10-11 05:07:23 -0300

    feat: create login page and components

--- no-panic/src/app/login/page.tsx
+++ no-panic/src/app/login/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from './styles.module.css';
+import { Metadata } from 'next';
+import { LoginForm } from '@/components/login-form';
+
+export const metadata: Metadata = {
+  title: 'No Panic | Login',
+};
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
