3bce621181e588b02584a8f1774596895e0d99ec
pedro-jp <joaopedroc035@gmail.com>
2025-11-05 22:42:59 -0300

    popup

--- no-panic/src/components/ui/header/index.tsx
+++ no-panic/src/components/ui/header/index.tsx
@@ -6,9 +6,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { IoClose, IoHeart, IoMenu } from 'react-icons/io5';
 import { AuthProvider, useAuth } from '@/context/auth-context';
+import { BiLogOut, BiUser } from 'react-icons/bi';
+import { redirect } from 'next/navigation';
 
 export const HeaderComponent = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
   return (
     <header className={styles.header}>
       <menu>
@@ -56,11 +58,20 @@ export const HeaderComponent = () => {
           </button>
           <div className={styles.config}>
             <Image src='/logo.png' alt='Perfil' width={20} height={20} />
-            {/* <ul className={styles.content}>
-              <li>oi</li>
-              <li>oi</li>
-              <li>oi</li>
-            </ul> */}
+            <ul className={styles.content}>
+              <li>
+                <Link href={'/perfil'}>
+                  <button>
+                    <BiUser /> Perfil
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <button onClick={() => logout()}>
+                  <BiLogOut /> Sair
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       </menu>
